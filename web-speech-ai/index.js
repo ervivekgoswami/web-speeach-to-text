8fa2e379ab0d1f5ce2de69e7486f92a4245e01e5
Vivek Kumar69 <Vivek.Kumar69@ad.infosys.com>
2020-09-03 15:25:21 +0530

    Adding to public repo

---
+++ web-speech-ai/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var apiai = require('apiai');
+var APIAI_TOKEN =apiai("5afc4bdf601046b39972ff3866cca392"); //use a api token from the official site
+const APIAI_SESSION_ID = "job-interview-moxv"; //use a session id
+
+console.log("my APIAI Toke is: ",APIAI_TOKEN);
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Get a reply from API.ai
+
+    let apiaiReq = APIAI_TOKEN.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
